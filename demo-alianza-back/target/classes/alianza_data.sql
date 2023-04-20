a3e3d76f49479aa47c644d3251fc0af7e9080813
ajax1987 <49579060+ajax1987@users.noreply.github.com>
2023-04-20 00:09:38 -0500

    Delete demo-alianza-back/target directory

--- demo-alianza-back/target/classes/alianza_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `person` (`shared_key`,`bussines_id`,`email`,`mobile_number`,`created_at`, `updated_at`)
-  VALUES ('jgutierrez','juliana gutierrez','jgutierrez@gmail.com','3219876543', NOW() ,NULL);
